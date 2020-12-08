1855389c824116fe47ae0b16504e3d96e65263c2
pphos <coc4h8conhc6h12nh@gmail.com>
2020-12-09 08:30:45 +0900

    fix HashRouter

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
   });
 
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <Switch>
         <Route exact path={appURL.top}>
           <GameTop
