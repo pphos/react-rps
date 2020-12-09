fff132d26356ec2942c995602e7504ad405d07a2
pphos <coc4h8conhc6h12nh@gmail.com>
2020-12-10 08:29:26 +0900

    Updates

--- precache-manifest.67cfda74cf48e9ac6c5499b025b64c6e.js
+++ precache-manifest.67cfda74cf48e9ac6c5499b025b64c6e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "741a63c3334c9c70d7d637822ed34ef3",
+    "revision": "2c6290491ecfb07cf0c3a0e1a6c50d2e",
     "url": "/react-rps/index.html"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-rps/static/js/2.77a1eea0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f5a45be62fe3b5075ee5",
-    "url": "/react-rps/static/js/main.d4f78836.chunk.js"
+    "revision": "f8d66eb6e7c7a2735413",
+    "url": "/react-rps/static/js/main.5b9d8336.chunk.js"
   },
   {
     "revision": "a02ae9f08a35c082f823",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-rps/precache-manifest.cfbe75d8d55eb8908fb6c4cb70fb952f.js"
+  "/react-rps/precache-manifest.67cfda74cf48e9ac6c5499b025b64c6e.js"
 );
 
 self.addEventListener('message', (event) => {
